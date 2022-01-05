c0dc66a61def8a463d6c3c2e7f35f1e261343db3
bculo <culobozo@gmail.com>
2022-01-05 13:00:55 +0100

    First Commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if(product is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
